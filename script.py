73f15c16d4d245dc88c13bc755a18d7ed32209e9
Guilherme <guilherme_trabalho@outlook.com.br>
2024-11-28 17:09:11 -0400

    Feat: Add conditional

--- script.py
+++ script.py
@@ -3,19 +3,20 @@ from rembg import remove
 from PIL import Image
 from Config import Input, Output
 
-
 teste_path = str(os.path.dirname(__file__) + Input)
 teste_dir = str(os.path.dirname(__file__) + Output)
 
-print("Iniciou")
+print("Início")
 path  = teste_path 
 new_dir = teste_dir 
 images = os.listdir(path)
 
-for image in images:
-    print(image)
-    input = Image.open(path + "/" + image)
-    output = remove(input)
-    output.save(new_dir + "/" + image)
-    
+if images:  
+    for image in images:
+        print(image)
+        input = Image.open(path + "/" + image)
+        output = output = remove(input, alpha_matting=True, alpha_matting_foreground_threshold=180,alpha_matting_background_threshold=30, alpha_matting_erode_size=5)
+        output.save(new_dir + "/" + image + ".png")
+else: 
+    print("Não há nenhuma imagem para tratar")    
 print("Fim")  
